0dbadfd13ea03bd3799bc072811ecb287459575c
Aditya Chaudhary <79193139+AdityaXChaudhary@users.noreply.github.com>
2025-11-24 19:40:11 +0530

    Add files via upload

---
+++ src/DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/StudentDB";
+    private static final String USER = "root";     
+    private static final String PASS = "adip2104"; 
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            e.printStackTrace(); 
+            return null;
+        }
+    }
+}
